179b5daef960908ebba778a6ab42f54f128ba769
iprijic <iprijic21@gmail.com>
2023-01-17 20:28:07 +0100

    npm update.

---
+++ nodeconsoleproject2/app.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Hello world from JS');
+console.log('Hello world from JS33445');
+console.log('Hello world from JS56726');
+//# sourceMappingURL=app.js.map
---
+++ nodeconsoleproject2/app.ts
@@ -0,0 +1,6 @@
+
+
+console.log('Hello world from JS');
+console.log('Hello world from JS33445');
+console.log('Hello world from JS56726');
+
